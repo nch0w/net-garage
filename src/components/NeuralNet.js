a5aba8b099ae6c4b47889bbb3c151db8cf09d944
Neil Chowdhury <neil.chowdhury@outlook.com>
2022-10-01 16:38:22 -0400

    add fancy model output plot

--- src/components/NeuralNet.js
+++ src/components/NeuralNet.js
@@ -4,8 +4,11 @@ import * as tfvis from "@tensorflow/tfjs-vis";
 import Plot from "react-plotly.js";
 import { generateLinearSeparable } from "../scripts/data";
 import { Button } from "@chakra-ui/react";
+import { model } from "@tensorflow/tfjs";
+import _ from "underscore";
 
-const colors = { 0: "red", 1: "blue" };
+const colors = { 0: "blue", 1: "red" };
+const n = 30;
 
 const NeuralNet = (props) => {
   const [points, setPoints] = useState({
@@ -14,29 +17,79 @@ const NeuralNet = (props) => {
     labels: [],
   });
 
-  const loadModel = () => {
+  const [model, setModel] = useState(null);
+  const [hmap, setHmap] = useState(tf.zeros([n, n]).add(0.5).arraySync());
+
+  const loadModel = async () => {
+    // create model (don't train on data yet though)
     console.log("NeuralNet useEffect");
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 1, inputShape: [2] }));
-    model.add(tf.layers.dense({ units: 1 }));
+    model.add(
+      tf.layers.dense({ units: 1, inputShape: [2], activation: "sigmoid" })
+    );
+    // model.add(tf.layers.dense({ units: 1 }));
 
-    console.log(JSON.stringify(model.outputs[0].shape));
+    // console.log(JSON.stringify(model.outputs[0].shape));
     // tfvis.show.modelSummary({name: 'Model Summary'}, model);
 
-    const { x, y } = generateLinearSeparable();
-    const pointsX = x.slice([0, 0], [x.shape[0], 1]).reshape([-1]).arraySync();
-    const pointsY = x.slice([0, 1], [x.shape[0], 1]).reshape([-1]).arraySync();
-    setPoints({ pointsX, pointsY, labels: y.reshape([-1]).arraySync() });
+    const { inputs, labels } = generateLinearSeparable();
+    const pointsX = inputs
+      .slice([0, 0], [inputs.shape[0], 1])
+      .reshape([-1])
+      .arraySync();
+    const pointsY = inputs
+      .slice([0, 1], [inputs.shape[0], 1])
+      .reshape([-1])
+      .arraySync();
+    setPoints({ pointsX, pointsY, labels: labels.reshape([-1]).arraySync() });
 
-    y.print();
-    console.log(y.reshape([-1]).arraySync());
-    console.log(points.labels.map((label) => colors[label]));
+    // compile model
+    model.compile({
+      optimizer: tf.train.sgd(0.2),
+      loss: tf.losses.meanSquaredError,
+      metrics: ["mse"],
+    });
+
+    const batchSize = 1;
+    const epochs = 100;
+
+    window.model = model;
+
+    // for (let i = 0; i < epochs; i++) {
+    //   const fit = await model.trainOnBatch(inputs, labels);
+    // }
+    const fit = await model.fit(inputs, labels, {
+      batchSize,
+      epochs,
+      shuffle: true,
+      callbacks: tfvis.show.fitCallbacks(
+        { name: "Training Performance" },
+        ["loss", "mse"],
+        { height: 200, callbacks: ["onEpochEnd"] }
+      ),
+    });
+
+    model.predict(inputs).print();
+    const hmapNew = tf.zeros([n, n]).arraySync();
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        hmapNew[i][j] = model
+          .predict(tf.tensor2d([[i / n, j / n]]))
+          .arraySync()[0][0];
+      }
+    }
+    setHmap(hmapNew);
+    setModel(model);
+
+    // y.print();
+    // console.log(y.reshape([-1]).arraySync());
+    // console.log(points.labels.map((label) => colors[label]));
   };
 
-  useEffect(loadModel, []);
+  useEffect(() => loadModel, []);
   return (
     <div>
-      <Button onClick={loadModel}>Reload</Button>
+      <Button onClick={loadModel}>Reload Data</Button>
       <div style={{ width: 300, height: 300 }}>
         <Plot
           data={[
@@ -59,6 +112,18 @@ const NeuralNet = (props) => {
           }}
           config={{ displayModeBar: false }}
         />
+      </div>
+      <div style={{ width: 300, height: 300 }}>
+        <Plot
+          data={[
+            {
+              z: hmap,
+              x: _.range(0, 1, 1 / n),
+              y: _.range(0, 1, 1 / n),
+              type: "heatmap",
+            },
+          ]}
+        ></Plot>
       </div>
     </div>
   );
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,14 +1,14 @@
 import * as tf from "@tensorflow/tfjs";
 
 export const generateLinearSeparable = () => {
-  const x = tf.round(tf.randomUniform([20, 2]).mul(100)).div(100);
+  const inputs = tf.round(tf.randomUniform([20, 2]).mul(100)).div(100);
 
   // separator
   const theta = tf.randomNormal([1, 2]);
-  const y = tf
-    .sign(x.add([-0.5, -0.5]).matMul(theta.transpose()))
+  const labels = tf
+    .sign(inputs.add([-0.5, -0.5]).matMul(theta.transpose()))
     .add(1)
     .div(2);
 
-  return { x, y };
+  return { inputs, labels };
 };
